7c4c128415ab829cf406ac3994684b776483faed
AOB-Creator <alpamisomirbekov630@gmail.com>
2021-08-09 11:43:45 +0500

    phenomenal

--- system.js
+++ system.js
@@ -1,4 +1,12 @@
+const alters = document.getElementById("alters")
+const menubar = document.querySelector(".menubar")
+alters.addEventListener("click", function(){
+    menubar.classList.remove("nonemenu")
+})
 
-const menubar = document.querySelector(".lest .first")
-const menu = document.querySelector(".menubar")
 
+const closeeyes = document.querySelector("#closeeyes");
+
+closeeyes.addEventListener("click", function(){
+    menubar.classList.add("nonemenu")
+})
